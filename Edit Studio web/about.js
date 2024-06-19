f21064c9ed07afb1620faeaed2b312e0fd36445f
sonuprsad <144135561+sonuprsad@users.noreply.github.com>
2024-06-19 20:04:46 +0530

    Add files via upload

---
+++ Edit Studio web/about.js
@@ -0,0 +1,6 @@
+function callfunctionfunction() {
+    alert("Congrat you subscribe our web  successfully")
+          }
+          document.getElementById('bookNowBtn').addEventListener('click', function() {
+            alert('You clicked the Book Now button!'); 
+          });

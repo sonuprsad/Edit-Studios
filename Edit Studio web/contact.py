dc04cd373597075f9995fe7e948106b23d5200a2
sonuprsad <144135561+sonuprsad@users.noreply.github.com>
2024-06-19 20:18:47 +0530

    Add files via upload

---
+++ Edit Studio web/contact.py
@@ -0,0 +1,3 @@
+import qrcode
+img=qrcode.make("D:\PROJect\Edit Studio web\contct.html")
+img.save("contac.png")
---
+++ Edit Studio web/home page.js
@@ -0,0 +1,6 @@
+  function myfunction() {
+  alert("Congrat you subscribe our web  successfully")
+        }
+        function callfunction() {
+            alert("please read work  page for more information ")
+                  }
